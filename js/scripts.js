04dd0091dca1bb3f033215986dfd4da9886a3288
Adam Smith and John Sawiris <adam@>
2017-10-31 15:37:52 -0700

    add styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,18 @@
-function Movie (name, imageSrc, imageAlt, rating, secondRun, description) {
+function Movie (name, imageSrc, ticketImgSrc, imageAlt, rating, secondRun, description) {
   this.name = name;
   this.imageSrc = imageSrc;
+  this.ticketImgSrc = ticketImgSrc;
   this.imageAlt = imageAlt;
   this.rating = rating;
   this.secondRun = secondRun;
   this.description = description;
 }
 
-var warDogs = new Movie ("War Dogs", "images/wardogs.jpg", "War Dogs Movie Poster", "R", false, "Based on the true story of two young men, David Packouz and Efraim Diveroli, who won a $300 million contract from the Pentagon to arm America's allies in Afghanistan.");
-var theBigShort = new Movie ("The Big Short", "images/thebigshort.jpg", "The Big Short Movie Poster", "R", false, "Four denizens in the world of high-finance predict the credit and housing bubble collapse of the mid-2000s, and decide to take on the big banks for their greed and lack of foresight.");
-var iceAge2 = new Movie ("Ice Age 2: The Meltdown", "images/iceage2.jpg", "Ice Age 2 Movie Poster", "PG", true, "Manny, Sid, and Diego discover that the ice age is coming to an end, and join everybody for a journey to higher ground. On the trip, they discover that Manny, in fact, is not the last of the woolly mammoths.");
-var theNotebook = new Movie ("The Notebook", "images/thenotebook.jpg", "The Notebook Movie Poster", "PG-13", true, "A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.");
-var dumbAndDumber = new Movie ("Dumb & Dumber", "images/dumbanddumber.jpg", "Dumb and Dumber Movie Poster",  "PG-13", true, "The cross-country adventures of two good-hearted but incredibly stupid friends.");
+var warDogs = new Movie ("War Dogs", "images/wardogs.jpg", "images/wardogs2.jpg", "War Dogs Movie Poster", "R", false, "Based on the true story of two young men, David Packouz and Efraim Diveroli, who won a $300 million contract from the Pentagon to arm America's allies in Afghanistan.");
+var theBigShort = new Movie ("The Big Short", "images/thebigshort.jpg", "images/thebigshort2.jpg", "The Big Short Movie Poster", "R", false, "Four denizens in the world of high-finance predict the credit and housing bubble collapse of the mid-2000s, and decide to take on the big banks for their greed and lack of foresight.");
+var iceAge2 = new Movie ("Ice Age 2: The Meltdown", "images/iceage2.jpg", "images/iceage22.jpg", "Ice Age 2 Movie Poster", "PG", true, "Manny, Sid, and Diego discover that the ice age is coming to an end, and join everybody for a journey to higher ground. On the trip, they discover that Manny, in fact, is not the last of the woolly mammoths.");
+var theNotebook = new Movie ("The Notebook", "images/thenotebook.jpg", "images/thenotebook2.jpg", "The Notebook Movie Poster", "PG-13", true, "A poor yet passionate young man falls in love with a rich young woman, giving her a sense of freedom, but they are soon separated because of their social differences.");
+var dumbAndDumber = new Movie ("Dumb & Dumber", "images/dumbanddumber.jpg", "images/dumbanddumber2.jpg", "Dumb and Dumber Movie Poster",  "PG-13", true, "The cross-country adventures of two good-hearted but incredibly stupid friends.");
 
 var moviesArray = [warDogs, theBigShort, iceAge2, theNotebook, dumbAndDumber];
 
@@ -62,16 +63,22 @@ $(function() {
     event.preventDefault();
     var userAge = parseInt($("#age").val());
     var userName = $("#name").val();
-    var movieTime = parseInt($("#movie-time").val());
-    var showTime = showingTime(movieTime);
-    var priceOfTicket = ticketPrice(movieTime, userAge, movieSelected.secondRun);
-    $("#user-input-form").slideUp();
-    $("div.movie-info").slideUp();
-    $("#ticket-display").slideDown();
-    $("#ticket-title-output").text("Thanks, " + userName + ". Enjoy the movie!");
-    $("#name-output").text("Name: " + userName);
-    $("#movie-title-output").text("Movie: " + movieSelected.name);
-    $("#showing-time-output").text("Showing Time: " + showTime);
-    $("#ticket-price-output").text("Price: $" + priceOfTicket + ".00");
+    if (userAge && userName) {
+      var movieTime = parseInt($("#movie-time").val());
+      var showTime = showingTime(movieTime);
+      var priceOfTicket = ticketPrice(movieTime, userAge, movieSelected.secondRun);
+      $("#user-input-form").slideUp();
+      $("div.movie-info").slideUp();
+      $("#ticket-display").slideDown();
+      $("#ticket-image").attr('src', movieSelected.ticketImgSrc);
+      $("#ticket-title-output").text("Thanks, " + userName + ". Enjoy the movie!");
+      $("#name-output").text("Name: " + userName);
+      $("#movie-title-output").text("Movie: " + movieSelected.name);
+      $("#showing-time-output").text("Showing Time: " + showTime);
+      $("#ticket-price-output").text("Price: $" + priceOfTicket + ".00");
+    } else {
+      alert("Please complete the ticket purchase form!");
+    }
+    
   });
 });
